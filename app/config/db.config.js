44ca60a93628c42dc2a9fe2fc625169f653a64af
Argemiro <argemirojunior123@gmail.com>
2023-06-27 08:07:32 -0300

    first

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "silly.db.elephantsql.com",
+  USER: "wndxxjie",
+  PASSWORD: "3iDf0UtANxWLbk1rxVLKFUKgzCMNkTmS",
+  DB: "wndxxjie",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
---
+++ migrations/20230627105605-tutorial.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tutorials', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tutorials');
+  }
+};
